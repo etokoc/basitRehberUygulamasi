426948dfa138c4ee92013061650edfb95807df68
Ertuğrul KOÇ <etoertugrul.koc@gmail.com>
2021-06-07 01:20:58 +0300

    commit

--- app/src/main/java/com/ertugrulkoc/rehber/KayitOlActivity.java
+++ app/src/main/java/com/ertugrulkoc/rehber/KayitOlActivity.java
@@ -1,14 +1,147 @@
 package com.ertugrulkoc.rehber;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class KayitOlActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+public class KayitOlActivity extends AppCompatActivity implements View.OnClickListener {
+    final static String XML_DOSYA_ADI = "kullaniciVeri";
+    final static String SUTUN_KULLANICI_AD = "kullanici";
+    private static final int REQUEST_STORAGE = 1;
+    String kullaniciAdi = "";
+    String kullaniciSifre = "";
+    Button kisiKaydetButton;
+    EditText kullaniciAdEdit, kullaniciSifreEdit;
+    Element password, name;
+    private String kullaniciAdCekilenVeri;
+    private String kullaniciSifreCekilenVeri;
+
+    public String getKullaniciAdCekilenVeri() {
+        return kullaniciAdCekilenVeri;
+    }
+
+    public void setKullaniciAdCekilenVeri(String kullaniciAdCekilenVeri) {
+        this.kullaniciAdCekilenVeri = kullaniciAdCekilenVeri;
+    }
+
+    public String getKullaniciSifreCekilenVeri() {
+        return kullaniciSifreCekilenVeri;
+    }
+
+    public void setKullaniciSifreCekilenVeri(String kullaniciSifreCekilenVeri) {
+        this.kullaniciSifreCekilenVeri = kullaniciSifreCekilenVeri;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kayit_ol);
+        kisiKaydetButton = findViewById(R.id.kisiKaydetButton);
+        kullaniciAdEdit = findViewById(R.id.kullaniciAdKayit);
+        kullaniciSifreEdit = findViewById(R.id.parolaKayit);
+        kisiKaydetButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        kullaniciAdi = kullaniciAdEdit.getText().toString();
+        kullaniciSifre = kullaniciSifreEdit.getText().toString();
+        if ( kullaniciAdi !=null && !kullaniciAdi.equals("") && kullaniciSifre !=null && !kullaniciSifre.equals("")){
+            xmlVeriEkle(kullaniciAdi, kullaniciSifre);
+
+        }else {
+            Toast.makeText(this, "Bilgileri Eksiksiz Giriniz !", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void xmlVeriEkle(String kullaniciAdi, String kullaniciSifre) {
+        try {
+            // Document olustur
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance()
+                    .newDocumentBuilder();
+            Document dom = builder.newDocument();
+
+            // root ekle
+            Element root = dom.createElement(SUTUN_KULLANICI_AD);
+            dom.appendChild(root);
+
+            // taglere ekle (name, password)
+            name = dom.createElement("kullaniciAd");
+            name.setTextContent(kullaniciAdi);
+            password = dom.createElement("sifre");
+            password.setTextContent(kullaniciSifre);
+            root.appendChild(name);
+            root.appendChild(password);
+
+            // Xmle yaz
+            if (ContextCompat.checkSelfPermission(KayitOlActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(KayitOlActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_STORAGE);
+            } else {
+                Transformer tr = TransformerFactory.newInstance().newTransformer();
+                tr.setOutputProperty(OutputKeys.INDENT, "yes");
+                tr.transform(new DOMSource(dom), new StreamResult(new File(android.os.Environment.getExternalStorageDirectory(), XML_DOSYA_ADI + ".xml")));
+                tr.transform(new DOMSource(dom), new StreamResult(System.out));
+                Intent intent = new Intent(this,MainActivity.class);
+                intent.putExtra("kulAd",kullaniciAdi);
+                startActivity(intent);
+                this.finish();
+            }
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void xmlVeriCek() {
+        try {
+            // Xmli doma cevir
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance()
+                    .newDocumentBuilder();
+            Document dom = builder.parse(new File(Environment.getExternalStorageDirectory(), XML_DOSYA_ADI + ".xml"));
+
+            // xml yapısı
+            dom.normalizeDocument();
+
+            // rootu al
+            Element root = dom.getDocumentElement();
+
+            // veriyi yazdir
+            setKullaniciAdCekilenVeri(root.getElementsByTagName("kullaniciAd").item(0).getTextContent());
+            setKullaniciSifreCekilenVeri(root.getElementsByTagName("sifre").item(0).getTextContent());
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/ertugrulkoc/rehber/MainActivity.java
+++ app/src/main/java/com/ertugrulkoc/rehber/MainActivity.java
@@ -1,27 +1,51 @@
 package com.ertugrulkoc.rehber;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    EditText kulAd, kulSifre;
+    CheckBox checkBox;
+    SharedPreferences sharedPreferences;
     private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       kisiyiHatirlamaDurumu();
+
         Button girisYapButton = findViewById(R.id.girisYapButton);
         girisYapButton.setOnClickListener(this);
-
+        checkBox = findViewById(R.id.checkBox);
+        kulAd = findViewById(R.id.editKullaniciAd);
+        kulSifre = findViewById(R.id.editKullaniciSifre);
         Button uyeOlButton = findViewById(R.id.kayitOlButton);
         uyeOlButton.setOnClickListener(this);
+        intent = getIntent();
+        String kisiAd = intent.getStringExtra("kulAd");
+        if (kisiAd != null) {
+            kulAd.setText(kisiAd);
+        }
 
+    }
 
+    private void kisiyiHatirlamaDurumu() {
+        sharedPreferences = getSharedPreferences("remember",MODE_PRIVATE);
+        if (sharedPreferences.getBoolean("remember",false)){
+            intent = new Intent(this.getApplicationContext(),Rehber_Activity.class);
+            startActivity(intent);
+            this.finish();
+        }
     }
 
     @Override
@@ -39,9 +63,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void girisYap() {
-        intent = new Intent(this.getApplicationContext(), Rehber_Activity.class);
-        startActivity(intent);
-        this.finish();
+        KayitOlActivity kayitOlActivity = new KayitOlActivity();
+        kayitOlActivity.xmlVeriCek();
+        String kullaniciAd = kayitOlActivity.getKullaniciAdCekilenVeri();
+        String kullaniciSifre = kayitOlActivity.getKullaniciSifreCekilenVeri();
+        String kullanici_ad_cekilen = String.valueOf(kulAd.getText());
+        String kullanici_sifre_cekilen = String.valueOf(kulSifre.getText());
+        if (kullaniciAd != null && kullaniciSifre != null && !kullaniciAd.equals("") && !kullaniciSifre.equals("")) {
+            if (kullaniciAd.equals(kullanici_ad_cekilen) && kullaniciSifre.equals(kullanici_sifre_cekilen)) {
+                checkBoxKontrolEt();
+                intent = new Intent(this.getApplicationContext(), Rehber_Activity.class);
+                startActivity(intent);
+                this.finish();
+            } else {
+                Toast.makeText(MainActivity.this, "Bilgilerinizi Kontrol Ediniz !", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "Lütfen Eksiksiz Giriniz !", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void checkBoxKontrolEt() {
+        if (checkBox.isChecked()) {
+            sharedPreferences = getSharedPreferences("remember", MODE_PRIVATE);
+            sharedPreferences.edit().putBoolean("remember", true).apply();
+        }
     }
 
     private void kayitOl() {
